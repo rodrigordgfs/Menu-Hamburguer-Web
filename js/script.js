44b16a4b84c4c5111490554547b46ae0d093cd30
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2021-05-31 11:41:26 -0300

    Function to hide menu on click

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 const btnMobile = document.getElementById('btn-mobile');
+const menu = document.getElementById('menu');
+const nav = document.getElementById('nav');
 
 function toogleMenu(event) {
     if (event.type === 'touchStart') event.preventDefault();
-    const nav = document.getElementById('nav');
     nav.classList.toggle('active');
     const active = nav.classList.contains('active');
     event.currentTarget.setAttribute('aria-expanded', active)
@@ -13,5 +14,12 @@ function toogleMenu(event) {
     }
 }
 
+function toogleItemMenu(event) {
+    if (event.type === 'touchStart') event.preventDefault();
+    nav.classList.toggle('active');
+}
+
 btnMobile.addEventListener('click', toogleMenu);
 btnMobile.addEventListener('touchStart', toogleMenu);
+menu.addEventListener('click', toogleItemMenu);
+menu.addEventListener('touchStart', toogleItemMenu);
